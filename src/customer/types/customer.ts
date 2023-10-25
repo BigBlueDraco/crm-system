78ce0b17c40da1f5018a3f9fff4cd1751ba57b3f
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-10-25 17:48:53 +0300

    try: remove user from customer

--- src/customer/types/customer.ts
+++ src/customer/types/customer.ts
@@ -1,5 +1,11 @@
 import { User } from '../../user/types/user';
 
-export interface Customer extends User {}
+export interface Customer {
+  firstName: string;
+  lastName: string;
+  middleName: string;
+  email: string;
+  phone: string;
+}
 
 export interface CustomerFilter extends Customer {}
