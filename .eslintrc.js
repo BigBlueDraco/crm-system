fcf7a064b8c84fbde4665dab60beb103a0d1d430
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-10-25 17:14:51 +0300

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'dist/**'],
   rules: {
     'prettier/prettier': [
       'error',
